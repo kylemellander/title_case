63430a565ab82550c5cd7a9a0f0016bc762f458c
Kellen Kolbeck & Kyle Mellander <student@epicodus.com>
2015-08-03 13:49:35 -0700

    Converted multiples of 3 to ping

---
+++ ping_pong/lib/ping_pong.rb
@@ -0,0 +1,20 @@
+class Fixnum
+  define_method(:ping_pong) do
+    a_array = []
+    self.times do |i|
+      if (i+1)%3==0
+        a_array.push("ping")
+      else
+        a_array.push((i+1).to_s)
+      end
+    end
+
+
+    output = ""
+    a_array.each do |n|
+      output.concat(n)
+    end
+    output
+
+  end
+end
--- ping_pong/spec/ping_pong_spec.rb
+++ ping_pong/spec/ping_pong_spec.rb
@@ -1,8 +1,16 @@
 require("rspec")
-require("ping_pong")
+require('ping_pong')
 
-describe('FixNum#ping_pong') do
+describe('Fixnum#ping_pong') do
   it("counts up to the number inputed") do
     expect(3.ping_pong()).to(eq("123"))
+  end
+
+  it("converts multiples of 3 to ping") do
+    expect(3.ping_pong()).to(eq("12ping"))
+  end
+
+  it("converts multiples of 5 to pong") do
+    expect(10.ping_pong()).to(eq("12ping4pongping78pingpong"))
   end
 end
